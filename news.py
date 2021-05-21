01866af2146ff71d2a1a19c66d74288df3381274
krishna jain <69294021+krishnajain@users.noreply.github.com>
2021-05-21 14:48:09 +0530

    Update news.py

--- news.py
+++ news.py
@@ -1,7 +1,7 @@
 import requests      
 from win32com.client import Dispatch
 def NewsFromBBC(): 
-    main_url = " https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=4dbc17e007ab436fb66416009dfb59a8"
+    main_url = " https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=YOUR API KEY"
     open_bbc_page = requests.get(main_url).json() 
     article = open_bbc_page["articles"] 
     results = []   
